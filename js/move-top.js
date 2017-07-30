9ba597a1d7936c5fb333f9275bf911f4a7c8fec7
rubyace71697 <kukreja34@hotmail.com>
2017-07-30 22:42:40 +0530

    minor updates

--- js/move-top.js
+++ js/move-top.js
@@ -7,20 +7,39 @@
             containerHoverIDHash = '#' + settings.containerHoverID;
         $('body').append('<a href="#" id="' + settings.containerID + '">' + settings.text + '</a>');
         $(containerIDhash).hide().on('click.UItoTop', function() {
+            console.log('------------------------------------');
+            console.log("hiding the button");
+            console.log('------------------------------------');
             $('html, body').animate({ scrollTop: 0 }, settings.scrollSpeed, settings.easingType);
+
             $('#' + settings.containerHoverID, this).stop().animate({ 'opacity': 0 }, settings.inDelay, settings.easingType);
+
+
+
+
             return false;
-        }).prepend('<span id="' + settings.containerHoverID + '"></span>').hover(function() { $(containerHoverIDHash, this).stop().animate({ 'opacity': 1 }, 600, 'linear'); }, function() { $(containerHoverIDHash, this).stop().animate({ 'opacity': 0 }, 700, 'linear'); });
+        }).prepend('<span id="' + settings.containerHoverID + '"></span>').hover(function() { $(containerHoverIDHash, this).stop().animate({ 'opacity': 1 }, 600, 'linear'); },
+
+            function() { $(containerHoverIDHash, this).stop().animate({ 'opacity': 0 }, 700, 'linear'); });
+
+
+
         $(window).scroll(function() {
             var sd = $(window).scrollTop();
             if (typeof document.body.style.maxHeight === "undefined") { $(containerIDhash).css({ 'position': 'absolute', 'top': sd + $(window).height() - 50 }); }
             if (sd > settings.min) {
                 $(containerIDhash).fadeIn(settings.inDelay);
                 $("#img-banner").fadeIn(settings.inDelay);
+                console.log('------------------------------------');
+                console.log("iding");
+                console.log('------------------------------------');
 
             } else {
+                console.log('------------------------------------');
+                console.log("showing");
+                console.log('------------------------------------');
                 $(containerIDhash).fadeOut(settings.Outdelay);
-                $("#img-banner").fadeOut(settings.Outdelay);
+                $("#img-banner").fadeOut(settings.inDelay);
 
             }
 
